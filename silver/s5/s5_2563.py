d709cee9784850fc998607c2e2e8ab58eacd1195
shinhaesol <shinhaesol11@gmail.com>
2024-04-03 01:09:24 +0900

    ac s5_2563.py

--- silver/s5/s5_2563.py
+++ silver/s5/s5_2563.py
@@ -1,13 +1,12 @@
 import sys
 
-
 t = int(input())
 matrix = [[0 for _ in range(100)] for _ in range(100)]
 
 for i in range(t):
     column, row = map(int, sys.stdin.readline().split())
-    for r in range(row-1, row+9):
-        for c in range(column-1, column+9):
+    for r in range(row - 1, row + 9):
+        for c in range(column - 1, column + 9):
             matrix[r][c] = 1
 
 area = 0
