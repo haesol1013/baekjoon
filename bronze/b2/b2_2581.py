de97035548131fbb96d76cfd11f0c7a143ee1263
shinhaesol <shinhaesol11@gmail.com>
2024-04-14 22:20:49 +0900

    ac b2_2581.py

---
+++ bronze/b2/b2_2581.py
@@ -0,0 +1,10 @@
+start = int(input())
+end = int(input())
+
+prime_num = [i for i in range(start, end+1) if 2 == len([j for j in range(1, i+1) if i % j == 0])]
+
+if len(prime_num) > 0:
+    print(sum(prime_num))
+    print(prime_num[0])
+else:
+    print(-1)
