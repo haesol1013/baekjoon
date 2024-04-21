237fd37354bbd2422546ea72fe175542db36c0f1
shinhaesol <shinhaesol11@gmail.com>
2024-04-21 10:00:32 +0900

    ac s5_10814.py

---
+++ silver/s5/s5_10814.py
@@ -0,0 +1,11 @@
+"""""""""
+# 나이순 정렬
+"""""""""
+
+import sys
+
+n = int(sys.stdin.readline())
+user = [tuple(sys.stdin.readline().split()) for _ in range(n)]
+user_new = list(map(lambda x: (int(x[0]), x[1]), user))
+for i in sorted(user_new, key=lambda x: x[0]):
+    print(*i)
