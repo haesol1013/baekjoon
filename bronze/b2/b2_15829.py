3c1352082936dfcc6bbbc1d10978a1972876da8a
haesol1013 <111187655+haesol1013@users.noreply.github.com>
2024-05-03 20:00:15 +0900

    Add b2_15829.py

---
+++ bronze/b2/b2_15829.py
@@ -0,0 +1,12 @@
+"""""""""
+#Hashing
+"""""""""
+
+length = int(input())
+sequence = input()
+alphabet = "_abcdefghijklmnopqrstuvwxyz"
+summation = 0
+for i in range(length):
+    index = alphabet.index(sequence[i])
+    summation += index * 31**i
+print(summation % 1234567891)
