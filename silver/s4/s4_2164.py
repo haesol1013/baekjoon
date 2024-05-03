aa139b42c8be19c3be2c067cb1220c318e41a418
haesol1013 <111187655+haesol1013@users.noreply.github.com>
2024-05-03 20:42:42 +0900

    Add s4_2164.py

---
+++ silver/s4/s4_2164.py
@@ -0,0 +1,11 @@
+"""""""""
+# 카드 2
+"""""""""
+from collections import deque
+
+n = int(input())
+deck = deque(list(range(1, n+1)))
+while len(deck) > 1:
+    deck.popleft()
+    deck.append(deck.popleft())
+print(*deck)
