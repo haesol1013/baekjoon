844f24948c0d18f8a1d4b7c36927174585810695
haesol1013 <111187655+haesol1013@users.noreply.github.com>
2024-05-04 14:49:16 +0900

    Add s4_10828.py

---
+++ silver/s4/s4_10828.py
@@ -0,0 +1,36 @@
+"""""""""
+# 스택
+"""""""""
+
+import sys
+
+
+def stack(order: str):
+    global arr
+    n = None
+    order = order.rstrip()
+
+    if " " in order:
+        order, n = order.split()
+
+    match order:
+        case ("push"):
+            arr.append(n)
+        case ("pop"):
+            return arr.pop() if arr else "-1"
+        case ("size"):
+            return str(len(arr))
+        case ("empty"):
+            return "0" if arr else "1"
+        case ("top"):
+            return arr[-1] if arr else "-1"
+
+
+n = int(sys.stdin.readline())
+arr = []
+for i in range(n):
+    order = sys.stdin.readline()
+    result = stack(order)
+
+    if result:
+        print(result)
