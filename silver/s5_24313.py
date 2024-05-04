6a09fb89ead09459cdf65d126e9f731ba87bcdf1
haesol1013 <111187655+haesol1013@users.noreply.github.com>
2024-05-04 19:48:50 +0900

    Add s5_24313.py

---
+++ silver/s5_24313.py
@@ -0,0 +1,14 @@
+"""""""""
+# 알고리즘 수업 - 점근적 표기 1
+"""""""""
+
+a1, a2 = map(int, input().split())
+c = int(input())
+n0 = int(input())
+
+if a1 > c and a2 < 0:
+    result = 0
+else:
+    result = 1 if a1*n0 + a2 <= c*n0 else 0
+
+print(result)
