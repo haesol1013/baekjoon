f62a88bb0035f92407645304d260b639d8662d63
haesol1013 <111187655+haesol1013@users.noreply.github.com>
2024-05-05 21:04:20 +0900

    Add b1_2309.py

---
+++ bronze/b1/b1_2309.py
@@ -0,0 +1,20 @@
+"""""""""
+# 일곱 난쟁이
+"""""""""
+
+import sys
+
+arr = [int(sys.stdin.readline().strip()) for _ in range(9)]
+
+for i in range(0, len(arr)-1):
+    for j in range(i+1, len(arr)):
+        short1 = arr[i]
+        short2 = arr[j]
+        summation = sum(arr) - (short1+short2)
+        if summation == 100:
+            arr.remove(short1)
+            arr.remove(short2)
+            break
+
+for i in sorted(arr):
+    print(i)
