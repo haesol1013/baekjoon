ae313e957894e5d93bd3bc03ff58983beaca76b2
haesol1013 <111187655+haesol1013@users.noreply.github.com>
2024-05-06 06:41:25 +0900

    Update b4_2480.py

--- bronze/b4/b4_2480.py
+++ bronze/b4/b4_2480.py
@@ -1,4 +1,11 @@
 # 주사위 세개 - 2480
 
+arr = list(map(int, input().split()))
+d1, d2, d3 = sorted(arr)
 
-
+if d1 == d2 == d3:
+    print(10000 + d3*1000)
+elif d1 != d2 != d3:
+    print(d3*100)
+else:
+    print(1000 + d2*100)
