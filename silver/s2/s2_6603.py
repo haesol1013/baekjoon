3fb2452915c29c40fec1d89580c55dc356843c90
haesol1013 <111187655+haesol1013@users.noreply.github.com>
2024-05-05 21:58:35 +0900

    Add s2_6603.py

---
+++ silver/s2/s2_6603.py
@@ -0,0 +1,16 @@
+"""""""""
+# 로또 
+"""""""""
+
+from itertools import combinations
+
+while True:
+    tmp = input()
+    if tmp == "0":
+        break
+    else:
+        _, *set_ = map(int, tmp.split())
+        for comb in combinations(set_, 6):
+            print(*comb)
+        print()
+
