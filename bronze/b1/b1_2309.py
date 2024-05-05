ffee71b95b17cd9d2b0df6332f10c1afa76e3ecd
haesol1013 <111187655+haesol1013@users.noreply.github.com>
2024-05-05 21:10:36 +0900

    Add ac code of b1_2309.py

--- bronze/b1/b1_2309.py
+++ bronze/b1/b1_2309.py
@@ -18,3 +18,14 @@ for i in range(0, len(arr)-1):
 
 for i in sorted(arr):
     print(i)
+
+
+# from itertools import combinations
+# import sys
+#
+# arr = [int(sys.stdin.readline().strip()) for i in range(9)]
+#
+# for comb in combinations(arr, 7):
+#     if sum(comb) == 100:
+#         print("\n".join(map(str, sorted(comb))))
+#         break
