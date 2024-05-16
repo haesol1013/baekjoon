4922309931c08bb59857fc7dd2e666479d3074de
Shin Haesol <111187655+haesol1013@users.noreply.github.com>
2024-05-16 19:58:38 +0900

    Update b3_10810.py

--- 0_bronze/b3/b3_10810.py
+++ 0_bronze/b3/b3_10810.py
@@ -1,22 +1,11 @@
+# 공 넣기 - 10810
+
 import sys
 
-
-def put(bundle, start, end, num):
-    stand_by = [i for i in bundle[start-1:end]]
-
-    for i in range(len(stand_by)):
-        stand_by[i] = num
-
-    bundle[start-1:end] = stand_by
-    return bundle
-
-
-N, M = map(int, sys.stdin.readline().split())
-box = [0 for _ in range(N)]
-
-for x in range(M):
+n, k = map(int, sys.stdin.readline().split())
+box = [0]*n
+for _ in range(4):
     i, j, k = map(int, sys.stdin.readline().split())
-    box = put(box, i, j, k)
-
-for x in box:
-    print(x, end=' ')
+    for x in range(i-1, j):
+        box[x] = k
+print(*box)
