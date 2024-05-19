a6ab370b950298ece0370717472212267d610abf
Shin Haesol <111187655+haesol1013@users.noreply.github.com>
2024-05-20 01:39:39 +0900

    Update s3_11478.py

--- 1_silver/s3/s3_11478.py
+++ 1_silver/s3/s3_11478.py
@@ -1,13 +1,12 @@
-# 서로 다른 부분 문자열의 개수 = 11478
+# 서로 다른 부분 문자열의 개수 - 11478
 
-from itertools import combinations
 import sys
 
 word = sys.stdin.readline().strip()
 
-cnt = 0
-for r in range(1, len(word)+1):
-    for comb in combinations(word, r):
-        cnt += 1
-        print(*comb, cnt)
-print(cnt)
+sub_str = []
+length = len(word)
+for i in range(1, length+1):
+    for j in range(length-i+1):
+        sub_str.append(word[j:j+i])
+print(len(set(sub_str)))
