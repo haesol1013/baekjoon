e241efe599c89aa5383cab642fc495593d4258a5
haesol1013 <shinhaesol11@gmail.com>
2024-08-17 00:03:25 +0900

    Update: s4_4134.py

--- 1_silver/s4/s4_4134.py
+++ 1_silver/s4/s4_4134.py
@@ -1,1 +1,24 @@
 # 다음 소수 - 4134
+
+def is_prime(n: int) -> bool:
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def main():
+    n = int(input())
+    while True:
+        if is_prime(n):
+            print(n)
+            return
+        else:
+            n += 1
+
+
+t = int(input())
+for i in range(t):
+    main()
