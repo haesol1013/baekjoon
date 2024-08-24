d63641f71d6e6f982dbca06ffff87b3ee1ef42d2
haesol1013 <shinhaesol11@gmail.com>
2024-08-25 00:53:03 +0900

    add: s4_4949.py

---
+++ 1_silver/s4/s4_4949.py
@@ -0,0 +1,38 @@
+# 균형잡힌 세상 - 4949
+
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def is_balanced(sentence: str) -> str:
+    stack = []
+    for char in sentence:
+        match char:
+            case "(":
+                stack.append(char)
+            case ")":
+                if stack and stack[-1] == "(":
+                    stack.pop()
+                else:
+                    stack.append(char)
+            case "[":
+                stack.append(char)
+            case "]":
+                if stack and stack[-1] == "[":
+                    stack.pop()
+                else:
+                    stack.append(char)
+            case _:
+                continue
+    return "no" if stack else "yes"
+
+
+def main():
+    while True:
+        str_ = input()
+        if str_ == ".":
+            break
+        print(is_balanced(str_))
+
+
+main()
