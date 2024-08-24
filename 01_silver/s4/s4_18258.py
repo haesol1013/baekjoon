52b572f204b618c5c215b20b4f2e60f98ebef598
haesol1013 <shinhaesol11@gmail.com>
2024-08-25 05:51:41 +0900

    edit: s4_18258

--- 01_silver/s4/s4_18258.py
+++ 01_silver/s4/s4_18258.py
@@ -1,22 +1,30 @@
 # 큐 2 - 18258
 
 import sys
+from collections import deque
 
-n = int(sys.stdin.readline())
-arr = []
-for _ in range(n):
-    order = sys.stdin.readline().strip().split()
-    match order[0]:
-        case "push":
-            arr.append(order[1])
-        case "pop":
-            print(arr.pop(0)) if arr else print(-1)
-        case "size":
-            print(len(arr))
-        case "empty":
-            print(0) if arr else print(1)
-        case "front":
-            print(arr[0]) if arr else print(-1)
-        case "back":
-            print(arr[-1]) if arr else print(-1)
 
+def main():
+    n = int(sys.stdin.readline())
+    arr = deque()
+
+    for _ in range(n):
+        order = sys.stdin.readline().strip().split()
+        match order[0]:
+            case "push":
+                arr.append(order[1])
+            case "pop":
+                print(arr.popleft()) if arr else print(-1)
+            case "size":
+                print(len(arr))
+            case "empty":
+                print(1) if not arr else print(0)
+            case "front":
+                print(arr[0]) if arr else print(-1)
+            case "back":
+                print(arr[-1]) if arr else print(-1)
+            case _:
+                continue
+
+
+main()
