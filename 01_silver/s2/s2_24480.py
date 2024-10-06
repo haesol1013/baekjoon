97ad2a30f503548b2e7c7ee9a495631269a59ff1
haesol1013 <shinhaesol11@gmail.com>
2024-10-06 22:54:26 +0900

    add: s2_24480.py

---
+++ 01_silver/s2/s2_24480.py
@@ -0,0 +1,31 @@
+# 알고리즘 수업 - 깊이 우선 탐색 2 - 24480
+
+import sys
+sys.setrecursionlimit(10**6)
+input = lambda: sys.stdin.readline().rstrip()
+
+
+order = 1
+def dfs(v):
+    global order
+    visited[v] = order
+    order += 1
+    for i in g[v]:
+        if not visited[i]:
+            dfs(i)
+
+
+n, m, r = map(int, input().split())
+g = [[] for _ in range(n+1)]
+visited = [0] * (n+1)
+for _ in range(m):
+    u, v = map(int, input().split())
+    g[u].append(v)
+    g[v].append(u)
+
+for i in g[1:]:
+    i.sort(reverse=True)
+
+dfs(r)
+
+print(*visited[1:], sep="\n")
